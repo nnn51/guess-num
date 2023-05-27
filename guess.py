93ecfb56b0be2b0aa7d75594fcde4ab63c14da8f
nnn51 <ceannn51@gmail.com>
2023-05-27 17:05:55 +0800

    decide  number range

--- guess.py
+++ guess.py
@@ -4,13 +4,18 @@
 # 猜錯 印出"比答案大/小"
 
 import random
-r = random.randint(1, 100)
+min = input('請決定隨機數字範圍最小值:')
+min = int(min)
+max = input('請決定隨機數字範圍最大值:')
+max = int(max)
+
+r = random.randint(min, max)
 count = 0  
 	# 要寫再while的外面，若寫在裡面，代表每進入while一次，count就又變回0
 
 while True:
 	count += 1   # count = count + 1 的速寫法
-	num = input('請輸入數字:')
+	num = input('請猜數字:')
 	num = int(num)
 	if num == r:
 		print('Bingo~! 答案是', r)
